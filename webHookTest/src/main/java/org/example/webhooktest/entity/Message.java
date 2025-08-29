2b02850e16724ba59df88663268f5a03f7b9ec47
나의찬 <cab07036@naver.com>
2025-08-30 00:05:52 +0900

    엔티티 dto 변ㄱㅇ

--- webHookTest/src/main/java/org/example/webhooktest/entity/Message.java
+++ webHookTest/src/main/java/org/example/webhooktest/entity/Message.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.example.webhooktest.dto.messageDto;
 
 @Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Builder
 public class Message {
 
     @Id
@@ -24,4 +27,26 @@ public class Message {
     private String author;
     private String date;
     private String url;
+    private String type;
+
+    public Message(String content, String title, String author, String date, String url, String type) {
+        this.content = content;
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.url = url;
+        this.type = type;
+    }
+
+    public messageDto entityToDto() {
+        return messageDto.builder()
+                .content(this.content)
+                .title(this.title)
+                .author(this.author)
+                .date(this.date)
+                .url(this.url)
+                .type(this.type)
+                .build();
+
+    }
 }
