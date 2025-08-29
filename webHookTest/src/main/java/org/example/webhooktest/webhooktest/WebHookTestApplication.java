89d63523b9fd8671746792bffaa26001def4cc6b
나의찬 <cab07036@naver.com>
2025-08-29 22:11:04 +0900

    init

---
+++ webHookTest/src/main/java/org/example/webhooktest/webhooktest/WebHookTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.webhooktest.webhooktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHookTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHookTestApplication.class, args);
+    }
+
+}
