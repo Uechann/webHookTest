b279a02521b67c22d5ae6dfe34eb3a14b8ad173e
나의찬 <cab07036@naver.com>
2025-09-14 03:39:53 +0900

    4개 파일 추가

---
+++ webHookTest/src/main/java/org/example/webhooktest/service/TestService4.java
@@ -0,0 +1,10 @@
+package org.example.webhooktest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService4 {
+    public String getTest4() {
+        return "test4";
+    }
+}
